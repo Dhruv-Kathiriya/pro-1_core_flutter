fa99a587d2921345ef7544cf0968ab295ddc5920
Dhruv kathiriya <150034575+Dhruv-Kathiriya@users.noreply.github.com>
2024-04-19 09:16:14 +0530

    Update 4.dart

--- 4.dart
+++ 4.dart
@@ -2,55 +2,63 @@
 3.) Write a Progaram to insert,Delete,Upadate,and View Oprestion on the elements in an array using menu driven progaramming approch. 
 */
 
+/*
+write a dart program to insert,update,delete, and view opration  on the elements in 
+an array using menu -driven programing approach
+*/
+
+
+
 import 'dart:io';
 
 void main() {
-  stdout.write("Enter size of in Array : ");
-  int size = int.parse(stdin.readLineSync()!);
+  var array = [1, 2, 3, 4];
+  List myArray = array.toList();
+  int choice;
 
-  List<int> a = List.generate(size, (index) => index);
+  do {
+    
+    print("press 1 to add  element   ");
+    print("press 2  Update  element   ");
+    print("press 3 to Remove  element  ");
+    print("press 4 to View  elements   ");
+    print("press 0 to Exit ");
+    stdout.write("Enter Your Choice : ");
+    choice = int.parse(stdin.readLineSync()!);
 
-  for (var i = 0; i < size; i++) {
-    stdout.write("Enter element of $i : ");
-    int val = int.parse(stdin.readLineSync()!);
-    a[i] = val;
-  }
-
-  print("1. Insert Value");
-  print("2. Delete Value");
-  print("3. Update Value");
-  print("4. View Value");
-  print("5. Exit");
-
-  stdout.write("Enter choice : ");
-  int choice = int.parse(stdin.readLineSync()!);
-
-  switch (choice) {
-    case 1:
-      stdout.write('insert value :');
-      int i = int.parse(stdin.readLineSync()!);
-      stdout.write('Enter value : ');
-      choice = int.parse(stdin.readLineSync()!);
-      a.insert(i, choice);
-      break;
-    case 2:
-      stdout.write('remove Value : ');
-      choice = int.parse(stdin.readLineSync()!);
-      a.remove(choice);
-      break;
-
-    case 3:
-      stdout.write(' update value : ');
-      int i = int.parse(stdin.readLineSync()!);
-      stdout.write('Enter value : ');
-      choice = int.parse(stdin.readLineSync()!);
-      a[i] = choice;
-      break;
-
-    case 4:
-      print('List : $a');
-
-    default:
-      print('Invalid choice!!');
-  }
+    switch (choice) {
+      case 1:
+        stdout.write("add Element  : ");
+        int addVar = int.parse(stdin.readLineSync()!);
+        myArray.add(addVar);
+        print(myArray);
+        break;
+      case 2:
+        stdout.write(" Update  Elements: ");
+        int index = int.parse(stdin.readLineSync()!);
+        print("");
+        stdout.write("Enter the Element  : ");
+        int newElements = int.parse(stdin.readLineSync()!);
+        myArray[index] = newElements;
+        print(myArray);
+        break;
+      case 3:
+        stdout.write(" Remove Elements : ");
+        int removeVar = int.parse(stdin.readLineSync()!);
+        myArray.remove(removeVar);
+        print(myArray);
+        break;
+      case 4:
+        stdout.write(" Viewing Elements  : ");
+        myArray.forEach((element) {
+          stdout.write("$element  ");
+        });
+        break;
+      case 0:
+        print("Exiting...");
+        break;
+      default:
+        print("Invalid Choice!!");
+    }
+  } while (choice != 0);
 }
