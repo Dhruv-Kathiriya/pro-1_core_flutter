877afa0d2b3d12c0c501faf3ba438f487e17a19f
Dhruv kathiriya <150034575+Dhruv-Kathiriya@users.noreply.github.com>
2024-04-19 09:23:04 +0530

    3.dart

--- 3.dart
+++ 3.dart
@@ -1,22 +1,41 @@
+/*
+4.) Write a dart progaram to addition of two matrices of dimenstion 3*3.
+*/
+
+import 'dart:io';
+
 void main() {
-  List a = [
-    [1, 2, 3],
-    [4, 5, 6]
+  List<List<int>> x = [
+    [11, 12, 13],
+    [14, 15, 16],
+    [17, 18, 19],
   ];
-  List b = [
-    [5, 5, 5],
-    [5, 5, 5]
-  ];
-  List c = [
-    [0, 0, 0],
-    [0, 0, 0]
+  List<List<int>> y = [
+    [11, 12, 13],
+    [14, 15, 16],
+    [17, 18, 19],
   ];
 
-  for (int i = 0; i <= 2; i++) {
-    for (int j = 0; j <= 2; j++) {
-      c[i][j] = a[i][j] + b[i][j];
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      stdout.write('Enter element of a[$i][$j] : ');
+      int val = int.parse(stdin.readLineSync()!);
+      x[i][j] = val;
     }
   }
 
-  print("$c");
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      stdout.write('Enter element of a[$i][$j] : ');
+      int val = int.parse(stdin.readLineSync()!);
+      x[i][j] = val;
+    }
+  }
+  print("sum is to");
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      stdout.write("${x[i][j] + y[i][j]} ");
+      print('');
+    }
+  }
 }
