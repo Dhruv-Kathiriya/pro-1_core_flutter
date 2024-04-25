bd3d7a35433db0d13e34b070b2ef3fbf64925e83
Dhruv kathiriya <150034575+Dhruv-Kathiriya@users.noreply.github.com>
2024-04-25 11:53:26 +0530

    Update 3.dart

--- 3.dart
+++ 3.dart
@@ -2,40 +2,44 @@
 4.) Write a dart progaram to addition of two matrices of dimenstion 3*3.
 */
 
-import 'dart:io';
+ import 'dart:io';
 
 void main() {
-  List<List<int>> x = [
-    [11, 12, 13],
-    [14, 15, 16],
-    [17, 18, 19],
-  ];
-  List<List<int>> y = [
-    [11, 12, 13],
-    [14, 15, 16],
-    [17, 18, 19],
-  ];
-
-  for (var i = 0; i < 3; i++) {
-    for (var j = 0; j < 3; j++) {
-      stdout.write('Enter element of a[$i][$j] : ');
-      int val = int.parse(stdin.readLineSync()!);
-      x[i][j] = val;
-    }
-  }
-
-  for (var i = 0; i < 3; i++) {
-    for (var j = 0; j < 3; j++) {
-      stdout.write('Enter element of a[$i][$j] : ');
-      int val = int.parse(stdin.readLineSync()!);
-      x[i][j] = val;
-    }
-  }
-  print("sum is to");
-  for (var i = 0; i < 3; i++) {
-    for (var j = 0; j < 3; j++) {
-      stdout.write("${x[i][j] + y[i][j]} ");
-      print('');
-    }
-  }
+  stdout.write("Enter Row of Numbers: ");
+  int row = int.parse(stdin.readLineSync()!);
+  print("");
+  stdout.write("Enter Column of Number : ");
+  int col = int.parse(stdin.readLineSync()!);
+  List<List<int>> myArray1 = List.generate(row, (i) {
+    return List.generate(col, (j) {
+      stdout.write("Enter value for element ${[i]} ${[j]}: ");
+      return int.parse(stdin.readLineSync()!);
+    });
+  });
+  List<List<int>> myArray2 = List.generate(row, (i) {
+    return List.generate(col, (j) {
+      stdout.write("Enter value for element ${[i]} ${[j]}: ");
+      return int.parse(stdin.readLineSync()!);
+    });
+  });
+  print("Array 1 : ");
+  myArray1.forEach((element) {
+    stdout.write("$element ");
+    print("");
+  });
+  print("Array 2 :");
+  myArray2.forEach((element) {
+    stdout.write("$element ");
+    print("");
+  });
+  print("Array 1 + Array 2 :");
+  List<List<int>> myArray3 = List.generate(row, (i) {
+    return List.generate(col, (j) {
+      return myArray1[i][j] + myArray2[i][j];
+    });
+  });
+  myArray3.forEach((element) {
+    stdout.write("$element ");
+    print("");
+  });
 }
